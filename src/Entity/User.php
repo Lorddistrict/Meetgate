55f78cf768601865ede95f97dc1d6d1a685b006a
Lorddistrict <etienne.crespi@laposte.net>
2019-06-24 16:11:41 +0200

    Fix circleCI

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -201,6 +201,4 @@ class User implements UserInterface
     {
         $this->isCertified = $isCertified;
     }
-
-
 }
--- src/Migrations/Version20190624135425.php
+++ src/Migrations/Version20190624135425.php
@@ -20,16 +20,26 @@ final class Version20190624135425 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
+                       'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL, CHANGE certified_token certified_token VARCHAR(255) DEFAULT NULL, CHANGE reset_token reset_token VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user 
+            CHANGE roles roles JSON NOT NULL, 
+            CHANGE certified_token certified_token VARCHAR(255) DEFAULT NULL, 
+            CHANGE reset_token reset_token VARCHAR(255) DEFAULT NULL
+        ');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
+                       'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COLLATE utf8mb4_bin, CHANGE certified_token certified_token VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE reset_token reset_token VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE user 
+            CHANGE roles roles LONGTEXT NOT NULL COLLATE utf8mb4_bin, 
+            CHANGE certified_token certified_token VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, 
+            CHANGE reset_token reset_token VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci
+        ');
     }
 }
