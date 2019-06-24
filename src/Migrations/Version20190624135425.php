f30a635fc15ad1a94ab8d36f937a8db444fa88bf
Lorddistrict <etienne.crespi@laposte.net>
2019-06-24 16:15:57 +0200

    Fix circleCI

--- src/Migrations/Version20190624135425.php
+++ src/Migrations/Version20190624135425.php
@@ -20,8 +20,10 @@ final class Version20190624135425 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
-                       'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
 
         $this->addSql('ALTER TABLE user 
             CHANGE roles roles JSON NOT NULL, 
@@ -33,8 +35,10 @@ final class Version20190624135425 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
-                       'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
 
         $this->addSql('ALTER TABLE user 
             CHANGE roles roles LONGTEXT NOT NULL COLLATE utf8mb4_bin, 
