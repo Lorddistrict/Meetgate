03a7893a9e589feed6153e9846da195e40af0527
Lorddistrict <etienne.crespi@laposte.net>
2019-06-26 20:33:14 +0200

    Add research bar - WIP

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,13 @@ namespace App\Controller;
 use App\Entity\Event;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/default", name="default")
-     */
     public function index(Request $request) : Response
     {
         $doctrine = $this->getDoctrine();
@@ -21,8 +19,8 @@ class HomeController extends AbstractController
         /** @var EventRepository $eventRepository */
         $eventRepository = $doctrine->getRepository(Event::class);
 
+        /** @var Event $events */
         $events = $eventRepository->findAll();
-
 
         return $this->render('home/home.html.twig', [
             'events' => $events,
