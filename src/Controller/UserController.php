7e1cecc68275c8b086e0db1fb5fea0641e667ae8
arthur.djikpo <arthur.djikpo@gmail.com>
2019-06-26 17:27:26 +0200

    login logout register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserLoginType;
+use App\Form\UserRegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
@@ -40,7 +42,29 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route(“/user/logout”, name=“logout”)
+     * @Route("/user/register", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserRegisterType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+        return $this->render(
+            'user/register.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+
+    /**
+     * @Route("/user/logout", name="logout")
      */
     public function logout()
     {
